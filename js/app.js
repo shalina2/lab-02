379f925360cad47c946806933bd7a5242b92ed17
shalom belaineh <shalom.belaineh@gmail.com>
2018-10-17 10:11:57 -0700

    got all images rendering

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-'use strict'
+'use strict';
 
 //constructor
-console.log('hello world');
+
 function Animal (animalobject){
     this.url =animalobject.image_url;
     this.title =animalobject.title;
@@ -9,16 +9,38 @@ function Animal (animalobject){
     this.horns=animalobject.horns;
     this.keyword=animalobject.keyword;
     allanimals.push(this);
-    console.log('creating animals');
+    
 }
     const allanimals =[];
 
-    const readJSON = function () {
+    Animal.prototype.render= function () {
+        $('main').append('<div class ="clone"></div>');
+        let $animalContainer =$('div[class="clone"]');
+        let $clonedanimal =$('#photo-template').html();
+        $animalContainer.html($clonedanimal);
+        // console.log($animalContainer);
+        $animalContainer.find('h2').text(this.title);
+        $animalContainer.find('img').attr('src',this.url);
+        $animalContainer.find('p').text(this.description);
+
+        $animalContainer.attr('class','');
+
+    }
+
+
+    let readJSON = function () {
         $.get('../data/page-1.json',data => {
             data.forEach(animaljson => {
                 new Animal(animaljson);
             })
-        }).then(console.log(allanimals))
+        }).then(renderallanimals)
+    }
+
+    function renderallanimals () {
+        allanimals.forEach(animal => {
+            animal.render();
+        })
     }
 
     readJSON();
+    //renderallanimals();
